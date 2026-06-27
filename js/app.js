2784b5c7a1c6c9ac56b3c673024dbd2afe24ef64
thanujaX <thanujahasaranga1@gmail.com>
2026-06-27 15:07:31 +0530

    Comment out conditional and loop examples for clarity

--- js/app.js
+++ js/app.js
@@ -27,40 +27,60 @@
 // let y = "11";
 // console.log(x=== y);
 
-let x = 10;
-let y = 20;
+// let x = 10;
+// let y = 20;
 
-if (x > y) {
-    console.log("x is greater than y");
-} else if (x < y) {
-    console.log("x is less than y");
-} else {
-    console.log("x is equal to y");
-}
+// if (x > y) {
+//     console.log("x is greater than y");
+// } else if (x < y) {
+//     console.log("x is less than y");
+// } else {
+//     console.log("x is equal to y");
+// }
 
-let day = 2;
-switch (day) {
-    case 1:
-        console.log("Monday");
-        break;
-    case 2:
-        console.log("Tuesday");
-        break;
-    case 3:
-        console.log("Wednesday");
-        break;
-    case 4:
-        console.log("Thursday");
-        break;
-    case 5:
-        console.log("Friday");
-        break;
-    case 6:
-        console.log("Saturday");
-        break;
-    case 7:
-        console.log("Sunday");
-        break;
-    default:
-        console.log("Invalid day");
-}
+// let day = 2;
+// switch (day) {
+//     case 1:
+//         console.log("Monday");
+//         break;
+//     case 2:
+//         console.log("Tuesday");
+//         break;
+//     case 3:
+//         console.log("Wednesday");
+//         break;
+//     case 4:
+//         console.log("Thursday");
+//         break;
+//     case 5:
+//         console.log("Friday");
+//         break;
+//     case 6:
+//         console.log("Saturday");
+//         break;
+//     case 7:
+//         console.log("Sunday");
+//         break;
+//     default:
+//         console.log("Invalid day");
+// }
+
+// for loop
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// while loop
+
+// let i = 0;
+// while (i <= 10) {
+//     console.log(i);
+//     i++;
+// }
+
+// do while loop
+let j = 0;
+do {
+    console.log(j);
+    j++;
+} while (j <= 10);
