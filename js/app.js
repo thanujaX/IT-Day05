52c04c3f9759e9b8a42731fea6855f649c444c31
thanujaX <thanujahasaranga1@gmail.com>
2026-06-27 14:58:11 +0530

    Comment out initial variable declarations and console logs for clarity

--- js/app.js
+++ js/app.js
@@ -1,10 +1,66 @@
-let name = "saman";
+// let name = "saman";
 
-let age = 23;
+// let age = 23;
 
-name = "ranil";
+// name = "ranil";
 
-console.log(name);
-console.log(age);
+// console.log(name);
+// console.log(age);
 
-alert("Hello icet" );
+// alert("Hello icet" );
+
+// let name = ("saman");
+// let age = 23;
+// let isAfterAL = true;
+// let customers = []
+// let customer = []
+// let salary = 10000.00;
+
+// console.log(typeof name);
+// console.log(typeof age);
+// console.log(typeof isAfterAL);
+// console.log(typeof customers);
+// console.log(typeof customer);
+// console.log(typeof salary);
+
+// let x = 10;
+// let y = "11";
+// console.log(x=== y);
+
+let x = 10;
+let y = 20;
+
+if (x > y) {
+    console.log("x is greater than y");
+} else if (x < y) {
+    console.log("x is less than y");
+} else {
+    console.log("x is equal to y");
+}
+
+let day = 2;
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+}
